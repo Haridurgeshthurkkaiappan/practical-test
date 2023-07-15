84f03833c39c7ed5f91cf140d6b4fe6ec04bd7cd
Hari Durgesh <haridurgesh.thurkkaiappan@gmail.com>
2023-07-15 12:31:27 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Data.SqlClient;
+using System.Linq;
+using Dapper;
+using CRUDlib;
+
+namespace CRUD1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RegisterationRepository obj = new RegisterationRepository();
+            obj.doit();
+        }
+    }
+}
